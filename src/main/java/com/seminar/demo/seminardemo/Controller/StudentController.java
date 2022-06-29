2a12725980ca6d22a93ecad2630512209c74b36a
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2022-06-29 09:06:37 -0500

    Controller Updates

--- src/main/java/com/seminar/demo/seminardemo/Controller/StudentController.java
+++ src/main/java/com/seminar/demo/seminardemo/Controller/StudentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:80", "https://monkfish-app-g4wbo.ondigitalocean.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:80", "https://www.app.gusmccoy.dev"})
 @RestController
 public class StudentController {
 
